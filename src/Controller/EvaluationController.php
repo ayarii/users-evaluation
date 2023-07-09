b42adc3bde90258e013350b31b600fba7c7ddf81
Fatma Belhaj <fatmazahra.benhajminiaoui@esprit.tn>
2023-07-09 15:00:09 +0100

    feat: making crud for evaluation and critere

---
+++ src/Controller/EvaluationController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Evaluation;
+use App\Form\EvaluationType;
+use App\Repository\EvaluationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/evaluation')]
+class EvaluationController extends AbstractController
+{
+    #[Route('/', name: 'app_evaluation_index', methods: ['GET'])]
+    public function index(EvaluationRepository $evaluationRepository): Response
+    {
+        return $this->render('evaluation/index.html.twig', [
+            'evaluations' => $evaluationRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_evaluation_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EvaluationRepository $evaluationRepository): Response
+    {
+        $evaluation = new Evaluation();
+        $form = $this->createForm(EvaluationType::class, $evaluation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $evaluationRepository->save($evaluation, true);
+
+            return $this->redirectToRoute('app_evaluation_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('evaluation/new.html.twig', [
+            'evaluation' => $evaluation,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_evaluation_show', methods: ['GET'])]
+    public function show(Evaluation $evaluation): Response
+    {
+        return $this->render('evaluation/show.html.twig', [
+            'evaluation' => $evaluation,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_evaluation_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Evaluation $evaluation, EvaluationRepository $evaluationRepository): Response
+    {
+        $form = $this->createForm(EvaluationType::class, $evaluation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $evaluationRepository->save($evaluation, true);
+
+            return $this->redirectToRoute('app_evaluation_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('evaluation/edit.html.twig', [
+            'evaluation' => $evaluation,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_evaluation_delete', methods: ['POST'])]
+    public function delete(Request $request, Evaluation $evaluation, EvaluationRepository $evaluationRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$evaluation->getId(), $request->request->get('_token'))) {
+            $evaluationRepository->remove($evaluation, true);
+        }
+
+        return $this->redirectToRoute('app_evaluation_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/EvaluationType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Evaluation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EvaluationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle')
+            ->add('description')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('enabled')
+            ->add('idUser')
+            ->add('idDepartement')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Evaluation::class,
+        ]);
+    }
+}
