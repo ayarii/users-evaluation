214628af70a4f294956f23bf81a907739cc8c313
malek-labidi <labidimalek@esprit.tn>
2023-07-10 13:45:50 +0100

    refactor : update crud

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 #[Route('/user')]
 class UserController extends AbstractController
@@ -22,15 +23,32 @@ class UserController extends AbstractController
     }
 
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, UserRepository $userRepository): Response
+    public function new(Request $request, UserRepository $userRepository, UserPasswordEncoderInterface $userPasswordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
-
+        //dd($user);
         if ($form->isSubmitted() && $form->isValid()) {
+            //dd($user);
+            $id = $form->get('id')->getData();
+            $imageFile = $form->get('image')->getData();
+            if ($imageFile) {
+                $imageData = $user->getNom().'-'.$user->getPrenom().'.'.$imageFile->guessExtension();
+                $imageFile->move(
+                    $this->getParameter('users_directory'),
+                    $imageData
+                );
+                $user->setImage($imageData);
+            }
+            $user->setPassword($userPasswordHasher->encodePassword(
+                $user,
+                $form->get('password')->getData()
+            ));
+            $user->setId($id);
+            //dd($user);
             $userRepository->save($user, true);
-
+            $this->addFlash('success', 'Utilisateur ajouté avec succés!');
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -49,14 +67,30 @@ class UserController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    public function edit(Request $request, User $user, UserRepository $userRepository, UserPasswordEncoderInterface $userPasswordHasher): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->save($user, true);
+            $imageFile = $form->get('image')->getData();
+            if ($imageFile) {
+                $imageData =$user->getNom().'-'.$user->getPrenom().'.'.$imageFile->guessExtension();
+                $imageFile->move(
+                    $this->getParameter('users_directory'),
+                    $imageData
+                );
+                $user->setImage($imageData);
+            }
+           
 
+            $user->setPassword($userPasswordHasher->encodePassword(
+                $user,
+                $form->get('password')->getData()
+            ));
+            //dd($user);
+            $userRepository->save($user, true);
+            $this->addFlash('success', 'Utilisateur mise a jour avec succés!');
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -72,7 +106,7 @@ class UserController extends AbstractController
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
             $userRepository->remove($user, true);
         }
-
+        $this->addFlash('success', 'Utilisateur supprimé avec succés!');
         return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
     }
 }
