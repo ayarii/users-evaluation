d3d3131271cad78793e88ca25f5198cbba70daae
malek-labidi <labidimalek@esprit.tn>
2023-07-21 00:27:34 +0100

    refactor : add image ,action to table badge

--- src/Form/BadgeType.php
+++ src/Form/BadgeType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
 
 class BadgeType extends AbstractType
 {
@@ -24,22 +26,32 @@ class BadgeType extends AbstractType
                 ],
             ]
             )
-            ->add('description',CKEditorType::class, array(
-                'config' => array(
-                    'uiColor' => '#ffffff',
-
-                    //...
-                ),
+            ->add('action',TextType::class,[
                 'attr' => [
                     'class' => 'form-control',
-                    'placeholder' => 'Description du badge...'
+                    'placeholder' => 'Action à faire poour obtenir le badge...'
                 ],
                 'invalid_message_parameters' => [
                     '%class%' => 'is-invalid',
                 ],
-
+            ]
+            )
+            ->add('image', FileType::class, [
+                'required'=>true,
+                'mapped' => true,
+                'label' => 'Image (JPG, PNG)',
+                
+                'attr' => [
+                    'accept' => '.jpg,.jpeg,.png',
+                    'placeholder' => 'choisir une image (JPG, PNG)',
+                ]
+            ])
+            ->add('description',CKEditorType::class, array(
+                'config' => array(
+                    'uiColor' => '#ffffff',
+                    //...
+                ),
             ))
-          
         ;
     }
 
