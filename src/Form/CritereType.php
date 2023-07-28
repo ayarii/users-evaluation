176a34db303cdeb07293a465fbca6349f61ef464
Fatma Belhaj <fatmazahra.benhajminiaoui@esprit.tn>
2023-07-28 10:57:48 +0100

    refactor : fixing duplicate error messages

--- src/Form/CritereType.php
+++ src/Form/CritereType.php
@@ -8,19 +8,33 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CritereType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('libelle')
-            ->add('ponderation')
+            ->add('libelle', TextType::class,  [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+              
+            ])
+            ->add('ponderation', TextType::class,  [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+              
+            ])
             ->add('description',CKEditorType::class, array(
                 'config' => array(
                     'uiColor' => '#ffffff',
                     //...
                 ),
+                'attr' => [
+                    'class' => 'form-control',
+                ],
             ))
 
            
--- src/Form/EvaluationType.php
+++ src/Form/EvaluationType.php
@@ -6,6 +6,8 @@ use App\Entity\Departement;
 use App\Entity\Evaluation;
 use App\Repository\DepartementRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
@@ -15,12 +17,20 @@ class EvaluationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('libelle')
+            ->add('libelle', TextType::class,  [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+              
+            ])
             ->add('description',CKEditorType::class, array(
                 'config' => array(
                     'uiColor' => '#ffffff',
                     //...
                 ),
+                'attr' => [
+                    'class' => 'form-control',
+                ],
             ))
          
             
